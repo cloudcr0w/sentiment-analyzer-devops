9ef06030302f79fe4ad2b40d8d706ff0c851ccd7
Adam Wrona <adamwronowy@gmail.com>
2025-05-05 13:58:31 +0800

    Terraform: define AWS region, instance type and key name variables

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name for SSH access"
+  type        = string
+}
